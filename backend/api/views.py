1d804eb34363c5e4e31705b89e223ab6ae20101b
hoot-dev <mhootman1980@gmail.com>
2022-05-07 23:05:40 -0500

    Django Rest Framework Generics RetrieveAPIView - 1:27:05

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,16 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-import json
+from products.models import Product
+from products.serializers import ProductSerializer
 
-
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
-    body = request.body
-    data = {}
-
-    print(request.GET)
-
-    try:
-        data = json.loads(body)
-    except:
-        data = {'error': 'whoops, could not parse the JSON'}
-    
-    print(data.keys())
-
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    
-    return JsonResponse(data)
+    """
+    DRF API View
+    """
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        #instance = serializer.save()
+        #print(instance)
+        return Response(serializer.data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = 'http://localhost:8000/api/'
 
-resp = requests.get(endpoint, params={'q': 'search'}, json={'something': 'nothing'})
+resp = requests.post(endpoint, json={'content': 'Hello world'})
 
 print(resp.text)
 print(f'status_code: {resp.status_code}')
